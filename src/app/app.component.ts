306e6430c3af838ed1c4a58fae080ca4521dc062
Walter Men <office@zeroooo.com.au>
2018-06-06 18:37:09 +1000

    first working version integrated with Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,36 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 
-import { HomePage } from '../pages/home/home';
-@Component({
+import { LoginPage } from '../pages/login/login';
+import { DataServiceProvider } from '../providers/data-service/data-service';
+import { TabsPage } from '../pages/tabs/tabs';
+@Component({  
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    public localStorage: Storage,
+    public dataService: DataServiceProvider,
+    platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+    });
+
+    localStorage.ready().then(() => {
+      this.dataService.load();
+
+      this.localStorage.get("loginstatus").then(status => {
+        this.rootPage = status? TabsPage : LoginPage;
+      });
+      //console.log("login status: ", this.dataService.isLogin());
+      //this.rootPage = this.dataService.isLogin()? TabsPage:LoginPage; 
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,76 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { TabsPage } from '../pages/tabs/tabs';
+import { JobPage } from '../pages/job/job';
+import { ServicePage } from '../pages/service/service';
+import { OrderPage } from '../pages/order/order';
+import { EditorPage } from '../pages/editor/editor';
+import { QuillModule } from 'ngx-quill';
+import { DataServiceProvider } from '../providers/data-service/data-service';
+import { ServiceData } from '../providers/service-data/service-data';
+import { OrderData } from '../providers/order-data/order-data';
+import { OrderCreatorPage } from '../pages/order-creator/order-creator';
+import { ServiceDetailsPage } from '../pages/service-details/service-details';
+import { JobData } from '../providers/job-data/job-data';
+import { AppSettings } from '../providers/app-setting';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    LoginPage,
+    TabsPage,
+    JobPage,
+    ServicePage,
+    OrderPage,
+    EditorPage,
+    OrderCreatorPage,
+    ServiceDetailsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpClientModule,
+    QuillModule,
+    IonicStorageModule.forRoot(),
+    IonicModule.forRoot(MyApp),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireModule.initializeApp(AppSettings.FIREBASE_CONFIG)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    LoginPage,
+    TabsPage,
+    JobPage,
+    ServicePage,
+    OrderPage,
+    EditorPage,
+    OrderCreatorPage,
+    ServiceDetailsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataServiceProvider,
+    ServiceData,
+    OrderData,
+    JobData,
+    AuthServiceProvider
   ]
 })
 export class AppModule {}
