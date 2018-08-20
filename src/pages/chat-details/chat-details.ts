1c900e6a7a83e259a7c0bc64de4fafe0519356c4
ykabcd <40310503+ykabcd@users.noreply.github.com>
2018-08-20 22:44:12 +1000

    chat details ui change

--- src/pages/chat-details/chat-details.ts
+++ src/pages/chat-details/chat-details.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { DataServiceProvider } from '../../providers/data-service/data-service';
 import { MessageData } from '../../providers/message-data/message-data';
+import { UserData } from '../../providers/user-data/user-data';
 
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
@@ -23,19 +24,25 @@ import firebase from 'firebase/app';
 export class ChatDetailsPage {
 
   chatItem: MessageData;
-
   itemsCollection: AngularFirestoreCollection<MessageData>; //Firestore collection
   items: Observable<MessageData[]>;
-
+  userDocument: AngularFirestoreDocument<UserData>;
+  currentUser: Observable<UserData>; 
+  currentUid: String;
+  message = new MessageData();
+  // postObj: PostData;
   constructor(
     private afs: AngularFirestore,
     public dataService: DataServiceProvider,
     public navCtrl: NavController, public navParams: NavParams) {
-
+      
+      this.userDocument = this.afs.doc<UserData>('users/' + firebase.auth().currentUser.uid);
+      this.currentUid = firebase.auth().currentUser.uid;
     if(this.navParams.data)
     {
       this.chatItem = this.navParams.get("sender");
     }
+    
   }
 
   ionViewDidLoad() {
@@ -50,5 +57,36 @@ export class ChatDetailsPage {
 
     this.items = this.itemsCollection.valueChanges();
   }
+ /*  onClick() {
 
+    console.log("message: ", this.message.message);
+    if(this.message.message)
+    {
+      this.message.receiver = firebase.auth().currentUser.uid;
+      this.message.sender = firebase.auth().currentUser.uid;
+      this.message.time = (new Date()).toString();
+
+      this.itemsCollection.doc(this.message.receiver)
+      .collection('chat').add({
+        "mid": "",
+        "time": this.message.time,
+        "sender": this.message.sender,
+        "snickname": this.dataService.userDataObj.nickname,
+        "savatar": this.dataService.userDataObj.avatar,
+        "receiver": this.message.receiver,
+        "message": this.message.message,
+        "status": "sent",
+        "sendAt": firebase.firestore.FieldValue.serverTimestamp()
+      }).then(docRef => {
+        this.itemsCollection.doc(this.message.receiver)
+        .collection('chat').doc(docRef.id).update({
+          "mid": docRef.id,
+          "updateAt": firebase.firestore.FieldValue.serverTimestamp()
+        });
+      })
+      .catch(error => console.log(error));
+
+
+    }
+  } */
 }
