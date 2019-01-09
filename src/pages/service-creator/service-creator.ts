5df52335a104b235ec8a55118e2fe1f5d0a1b44e
ykabcd <40310503+ykabcd@users.noreply.github.com>
2019-01-09 12:06:28 +1100

    yk-09012019-patch up

--- src/pages/service-creator/service-creator.ts
+++ src/pages/service-creator/service-creator.ts
@@ -30,45 +30,7 @@ export class ServiceCreatorPage {
   itemsCollection: AngularFirestoreCollection<ServiceData>; //Firestore collection
   itemDocument: AngularFirestoreDocument<ServiceData>; // read collection
   types: string[] = ['Escort', 'Transport', 'House Clean'];
-  unitJson = {
-    number: [
-      {description:'1'},
-      {description:'2'},
-      {description:'3'},
-      {description:'4'},
-      {description:'5'},
-      {description:'6'},
-      {description:'7'},
-      {description:'8'},
-      {description:'9'},
-      {description:'10'},
-      {description:'11'},
-      {description:'12'},
-      {description:'13'},
-      {description:'14'},
-      {description:'15'},
-      {description:'16'},
-      {description:'17'},
-      {description:'18'},
-      {description:'19'},
-      {description:'20'},
-      {description:'21'},
-      {description:'22'},
-      {description:'23'},
-      {description:'24'},
-      {description:'25'},
-      {description:'26'},
-      {description:'27'},
-      {description:'28'},
-      {description:'29'},
-      {description:'30'},
-    ],
-    units: [
-      {description: 'Minutes'},
-      {description: 'Hours'},
-      {description: 'Days'},
-    ]
-  }
+  units: string[] = ['Minutes','Hours', 'Weeks'];
   currencys: string[] = ['AUD','RMB','USD'];
   constructor(
     private selector: WheelSelector,
